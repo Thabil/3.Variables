d09c2f6af6d73e86080abae1526eab3fe8e7c351
A253875 <A253875@LSBSA-65LG3J3>
2024-01-10 16:20:22 +0200

    Add project files.

---
+++ 3.Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare variable
+            string name;
+            int age;
+
+            //initialize variables
+            name = "Thabile";
+            age = 30;
+            Console.WriteLine("Hi "+name);
+            Console.WriteLine("You are "+age+" years of age.");
+            Console.WriteLine("Next year you will be " + (age + 1)); 
+            
+
+            age = 30 + 10;
+            Console.WriteLine("Next 10 years time you will be " + age);
+
+            //declare and initialize in one line
+            string lastName = "Latakgomo";
+            Console.WriteLine(" Your last name is " + lastName);
+
+            //console freeze
+            Console.ReadLine();
+        }
+    }
+}
